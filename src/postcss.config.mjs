7815bda3141a15d0c866cce2d1227af39ef8b945
VicSDN <victormenendez2011@gmail.com>
2025-02-06 14:05:48 +0100

    Header screen 100 funcionalityy

--- src/postcss.config.mjs
+++ src/postcss.config.mjs
@@ -2,7 +2,7 @@ module.exports = {
     plugins: [
       require('tailwindcss'),
       require('autoprefixer'),
-      require('postcss-nested') // Si estás usando anidación CSS
+      require('postcss-nested') 
     ]
   };
   
