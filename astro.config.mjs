4eba1ecae622d7482881864b7b596d10c8acca5b
VicSDN <victormenendez2011@gmail.com>
2025-02-06 12:43:01 +0100

    changes in dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-    vite:{
-        server:{
-            watch : {
-                usePolling: true,
-                interval:100,
-            },
-        },
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{astro,html,js,ts,jsx,tsx}',  // Asegúrate de incluir las extensiones correctas
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
