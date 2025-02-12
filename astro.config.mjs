9dfd8887333e430074a967b45d8381247d850993
VicSDN <victormenendez2011@gmail.com>
2025-02-12 12:56:23 +0100

    Fix bug in year selector

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 
+import vercel from '@astrojs/vercel';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [ tailwind()],
-  output: 'server',
-  prefetch: true ,
+  prefetch: true,
+  adapter: vercel(),
 });
